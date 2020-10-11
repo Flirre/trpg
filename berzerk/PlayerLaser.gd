ae6e1d1fc3484d5aa3e38a02bc1acc78e803869d
Filip Lindahl <filip.g.lindahl@gmail.com>
2020-10-11 19:04:48 +0200

    broke out TRPG into own repo

--- berzerk/PlayerLaser.gd
+++
@@ -1,7 +0,0 @@
-extends "res://Laser.gd"
-
-func _on_PlayerLaser_body_entered(body):
-    disappear()
-    if(body.has_node('entity')):
-        body.get_node('entity').take_damage(damage)
-        body.get_node('entity').knockback(self.transform.origin - body.transform.origin, knockback)
