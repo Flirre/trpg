4df3f483bb3bc1bfb9d9c08a5ac7560bde319f4d
Filip Lindahl <filip.g.lindahl@gmail.com>
2021-03-07 17:38:07 +0100

    fix minor mistake with sprite direction

--- Character.gd
+++ Character.gd
@@ -272,7 +272,7 @@ func set_direction():
 		set_sprite_direction(0, 0)
 		emit_signal("direction_set")
 	elif Input.is_action_pressed("ui_left"):
-		set_sprite_direction(1, 1)
+		set_sprite_direction(1, 0)
 		emit_signal("direction_set")
 
 
